22bec2d3e6e9482e106dfe06b75f223da4189823
freedommk <mari.kuprava@gmail.com>
2023-09-11 19:03:32 -0700

    async function added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () {
+  const res = await fetch("https://picsum.photos/v2/list?limit=100");
+  const images = await res.json();
+  selectRandomImage(images);
+};
+const selectRandomImage = function (images) {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  console.log(randomIndex);
+};
